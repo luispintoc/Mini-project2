81fb59e34836a0a9ff180f190e70559ae8252442
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-02-07 11:46:58 -0500

    Update Mini-project2_no_extra.py

--- Mini-project2_no_extra.py
+++ Mini-project2_no_extra.py
@@ -9,6 +9,8 @@ from nltk.corpus import stopwords
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Normalizer
 import warnings
 warnings.filterwarnings("ignore",category = FutureWarning)
 filter_words = []
@@ -39,12 +41,27 @@ def compile(reviews):
 	return reviews
 
 def vectorization(train,test, words):			#It will vectorize the train set and it will transform both train and test set
-	cv = CountVectorizer(binary = True, stop_words = words)
+	cv = CountVectorizer(binary = True, stop_words = words, min_df = 0.01)
 	cv.fit(train)
 	train = cv.transform(train)
 	test = cv.transform(test)
 	return train, test
 
+def get_stemmed_text(corpus,name): #PorterStemmer - SnowballStemmer("english")
+	if name == 'Porter':
+		from nltk.stem.porter import PorterStemmer
+		stemmer = PorterStemmer()
+	else:
+		from nltk.stem.snowball import SnowballStemmer
+		stemmer = SnowballStemmer("english")
+	return [' '.join([stemmer.stem(word) for word in review.split()]) for review in corpus]
+
+def get_lemmatized_text(corpus):
+    import nltk
+    nltk.download('wordnet')
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
+    return [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in corpus] 
 
 #		*********Validation Pipeline******
 # def validation(classifier,x,y,k_fold):
@@ -56,7 +73,15 @@ def vectorization(train,test, words):			#It will vectorize the train set and it 
 #		*********Classifiers*******
 
 x_train,x_val,y_train,y_val = train_test_split(compile(reviews), target, train_size = 0.75, random_state = 42)
+# x_train = get_stemmed_text(x_train,'Porter')
+# x_val = get_stemmed_text(x_val,'Porter')
+# x_train = get_stemmed_text(x_train,'Snow')
+# x_val = get_stemmed_text(x_val,'Snow')
+x_train = get_lemmatized_text(x_train)
+x_val = get_lemmatized_text(x_val)
 [x_train,x_val] = vectorization(x_train, x_val, filter_words)
+
+
 
 print('Classifier: LogisticRegression')
 # for c in [0.01, 0.05, 0.25, 0.5, 1]:
@@ -66,7 +91,8 @@ print('Classifier: LogisticRegression')
 
 lr = LogisticRegression(C = 0.05)
 lr.fit(x_train,y_train)
-print("Accuracy using C = 0.05: %s " %(accuracy_score(y_val,lr.predict(x_val))))
+print("Accuracy on train set using C = 0.05: %s " %(accuracy_score(y_train,lr.predict(x_train))))
+print("Accuracy on val set using C = 0.05: %s " %(accuracy_score(y_val,lr.predict(x_val))))
 
 
 print('Classifier: Support Vector Machines')
@@ -76,18 +102,23 @@ print('Classifier: Support Vector Machines')
 # 	print("Accuracy for C=%s: %s" %(c,accuracy_score(y_val,svm.predict(x_val))))
 
 svm = LinearSVC(C = 0.01)
+
+#svm = Pipeline((("scaler", Normalizer()),("linear_svc", LinearSVC(C=0.01, loss="hinge")),))
 svm.fit(x_train,y_train)
-print("Accuracy using C = 0.01: %s " %(accuracy_score(y_val,svm.predict(x_val))))
+print("Accuracy on train set using C = 0.01: %s " %(accuracy_score(y_train,svm.predict(x_train))))
+print("Accuracy on val set using C = 0.01: %s " %(accuracy_score(y_val,svm.predict(x_val))))
 
-print('Classifier: Random Forest')
-forest_reg = RandomForestClassifier()
-forest_reg.fit(x_train,y_train)
-print("Accuracy: ", accuracy_score(y_val,forest_reg.predict(x_val)))
+# print('Classifier: Random Forest')
+# forest_reg = RandomForestClassifier()
+# forest_reg.fit(x_train,y_train)
+# print("Accuracy on train set using C = 0.01: %s " %(accuracy_score(y_train,forest_reg.predict(x_train))))
+# print("Accuracy on val set: ", accuracy_score(y_val,forest_reg.predict(x_val)))
 
-print('Classifier: Decision Tree')
-tree_reg = DecisionTreeClassifier()
-tree_reg.fit(x_train,y_train)
-print("Accuracy: ", accuracy_score(y_val,tree_reg.predict(x_val)))
+# print('Classifier: Decision Tree')
+# tree_reg = DecisionTreeClassifier()
+# tree_reg.fit(x_train,y_train)
+# print("Accuracy on train set: %s " %(accuracy_score(y_train,tree_reg.predict(x_train))))
+# print("Accuracy on val set: ", accuracy_score(y_val,tree_reg.predict(x_val)))
 
 
 
@@ -110,5 +141,3 @@ print("Accuracy: ", accuracy_score(y_val,tree_reg.predict(x_val)))
 #     feature_to_coef.items(), 
 #     key=lambda x: x[1])[:10]:
 #     print (best_negative)
-
-
