03bd574b8f4e31b2721e343348c3f154c335e1b7
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-02-06 19:30:48 -0500

    Update Mini-project2_no_extra.py

--- Mini-project2_no_extra.py
+++ Mini-project2_no_extra.py
@@ -8,7 +8,7 @@ reviews = [] #Shuffled training data, len = 25000
 
 with open("train_data.json") as fp:
     train_data = json.load(fp)
-
+#print(target[:20])
 
 i = 0
 while i < 25000:
@@ -16,22 +16,24 @@ while i < 25000:
 	reviews.append(train_data[i][1])
 	i += 1
 
-#print(len(target))
-#print(len(reviews))
 
+#		********Preprocessing********
 
 delete = re.compile("(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
 replace_with_space = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
 
-def preprocessing(reviews):
+def compile(reviews):
 	reviews = [delete.sub("",line.lower()) for line in reviews]
 	reviews = [replace_with_space.sub(" ",line) for line in reviews]
+	return reviews
+
+def vectorization(train,test):			#It will vectorize the train set and it will transform both train and test set
 	cv = CountVectorizer(binary = True)
+	cv.fit(train)
+	train = cv.transform(train)
+	test = cv.transform(test)
 
 
 
 	return reviews
-
-
-
 
