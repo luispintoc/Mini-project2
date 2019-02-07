cb5ab97a7157c7fa2d5a482e59686a867e2641a1
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-02-06 19:11:06 -0500

    Update read.py

--- read.py
+++ read.py
@@ -1,19 +1,17 @@
-import os
+import os, json
 from random import shuffle
 import numpy as np
-import json
 
 reviews_train = []  #first 12500 are positive reviews, the rest are negative
 reviews_test = []
 train_pos_path = 'train//pos'
 train_neg_path = 'train//neg'
 test_path = 'test'
-path = 'words.txt'
-word_file = open(path, 'w')
+train_shuffled = open('train_shuffled.txt','w')
+
 
 for file in os.listdir(train_pos_path):
 	with open(os.path.join(train_pos_path,file),"r",encoding="utf8") as f:
-		#print(file)
 		reviews_train.append(f.read())
 
 for file in os.listdir(train_neg_path):
@@ -21,20 +19,15 @@ for file in os.listdir(train_neg_path):
 		reviews_train.append(f.read())
 #print(len(reviews_train))
 
-#for file in os.listdir(test_path):
-	#with open(os.path.join(test_path,file),"r",encoding="utf8") as f:
-		#reviews_test.append(f.read())
+# for file in os.listdir(test_path):
+# 	with open(os.path.join(test_path,file),"r",encoding="utf8") as f:
+# 		reviews_test.append(f.read())
 #print(len(reviews_test))
 
 target_train = [1 if i<12500 else 0 for i in range(25000)]
 
 train_set = list(zip(target_train,reviews_train))
 shuffle(train_set)
-# print(train_set[0][0])
-# print(train_set[0][1])
 
-for value,comment in train_set:
-	word_file.write(str(value))
-	word_file.write(comment + ",,,")
-
-word_file.close()
+with open('train_data.json','w') as outfile:
+	json.dump(train_set,outfile)
