263460f1c52a7ccf2e1c3a77cd8fdc4bc4b18c37
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-02-08 00:46:28 -0500

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -4,30 +4,35 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from nltk.corpus import stopwords
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import Normalizer
+from sklearn.feature_extraction.text import TfidfTransformer
 import warnings
-warnings.filterwarnings("ignore",category = FutureWarning)
-filter_words = []
 
+warnings.filterwarnings("ignore",category = FutureWarning)
+#warnings.filterwarnings("ignore")
+
+#		*****Initialization*****
+
+filter_words = []
 target = [] #len = 25000
 reviews = [] #Shuffled training data, len = 25000
 
-with open("train_data.json") as fp:
+
+#		********Read data*********
+
+with open("train_data2.json") as fp:
     train_data = json.load(fp)
-
-
 i = 0
 while i < 25000:
 	target.append(train_data[i][0])
 	reviews.append(train_data[i][1])
 	i += 1
-#print(target[:20])
 
 
 #		********Preprocessing********
@@ -40,12 +45,6 @@ def compile(reviews):
 	reviews = [replace_with_space.sub(" ",line) for line in reviews]
 	return reviews
 
-def vectorization(train,test, words):			#It will vectorize the train set and it will transform both train and test set
-	cv = CountVectorizer(binary = True, stop_words = words, min_df = 0.01)
-	cv.fit(train)
-	train = cv.transform(train)
-	test = cv.transform(test)
-	return train, test
 
 def get_stemmed_text(corpus,name): #PorterStemmer - SnowballStemmer("english")
 	if name == 'Porter':
@@ -63,38 +62,42 @@ def get_lemmatized_text(corpus):
     lemmatizer = WordNetLemmatizer()
     return [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in corpus] 
 
-#		*********Validation Pipeline******
-# def validation(classifier,x,y,k_fold):
-# 	accuracy = cross_val_score(classifier,x,y,cv = k_fold)
-# 	print('Accuracies for a ',k_fold,'fold cross validation:')
-# 	return accuracy
 
+#		*******Feature pipelines******
 
-#		*********Classifiers*******
+vect_pipeline = Pipeline([('vect', CountVectorizer(ngram_range=(1,2))),
+							('tfidf', TfidfTransformer()),
+							('classifier', LogisticRegression())])
 
-x_train,x_val,y_train,y_val = train_test_split(compile(reviews), target, train_size = 0.75, random_state = 42)
+#       *********Applying preprocessing*******
+
+reviews = compile(reviews)
+#x_train,x_val,y_train,y_val = train_test_split(compile(reviews), target, train_size = 0.75, random_state = 42)
 # x_train = get_stemmed_text(x_train,'Porter')
 # x_val = get_stemmed_text(x_val,'Porter')
-# x_train = get_stemmed_text(x_train,'Snow')
-# x_val = get_stemmed_text(x_val,'Snow')
-x_train = get_lemmatized_text(x_train)
-x_val = get_lemmatized_text(x_val)
-[x_train,x_val] = vectorization(x_train, x_val, filter_words)
+x_train = get_stemmed_text(x_train,'Snow')
+x_val = get_stemmed_text(x_val,'Snow')
+# x_train = get_lemmatized_text(x_train)
+# x_val = get_lemmatized_text(x_val)
+#[x_train,x_val] = tf_idf_vectorization(x_train, x_val)
 
 
+#		*********Grid Search*******
+parameters_grid = {'vect__binary': (True,False),
+					'vect__min_df':(30,50,100,120),
+					'tfidf__use_idf': (True, False),
+					'classifier__penalty':('l1','l2'),
+					'classifier__C':(0.05,0.15)}
+# parameters_grid = {'vect__binary': (True,False),
+# 					}
 
-print('Classifier: LogisticRegression')
-# for c in [0.01, 0.05, 0.25, 0.5, 1]:
-# 	lr = LogisticRegression(C=c)
-# 	lr.fit(x_train,y_train)
-# 	print("Accuracy for C=%s: %s" %(c,accuracy_score(y_val,lr.predict(x_val))))
+#		*********Validation Pipeline*******
 
-lr = LogisticRegression(C = 0.05)
-lr.fit(x_train,y_train)
-print("Accuracy on train set using C = 0.05: %s " %(accuracy_score(y_train,lr.predict(x_train))))
-print("Accuracy on val set using C = 0.05: %s " %(accuracy_score(y_val,lr.predict(x_val))))
-
-
+grid_search = GridSearchCV(vect_pipeline, parameters_grid, cv=4, n_jobs=-2, scoring='accuracy')
+grid_search.fit(reviews,target)
+cvres = grid_search.cv_results_
+for accuracy, params in zip(cvres['mean_test_score'],cvres['params']):
+	print('Mean accuracy: ', accuracy,'  using: ',params)
 
 
 
