4ed401a81ad0221733532791e60b3f9f111d188e
johnmcogwan1 <john.mcgowan@mail.mcgill.ca>
2019-02-11 01:19:46 -0500

    Now does classification on a validation set and prints out error rate, accuracy, recall, precision, and specificity. Still have a bug to fix here

--- bin_naive_bayes.py
+++ bin_naive_bayes.py
@@ -1,18 +1,10 @@
-#load json file
-from google.colab import files
-files.upload()
-# choose the file on your computer to upload it then
-
-# Note that this does not work on Firefox for some reason, according to stack overflow
-
-#load positive word list
-from google.colab import files
-files.upload()
-
 import os, re, json
 from random import shuffle
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
+import nltk.sentiment as nltk
+import math as math
+#import python3_sklearn
+#from sklearn.feature_extraction.text import CountVectorizer
 
 target = [] #len = 25000
 reviews = [] #Shuffled training data, len = 25000
@@ -27,10 +19,6 @@ while i < 25000:
         reviews.append(train_data[i][1])
         i += 1
 
-print(len(target))
-print(len(reviews))
-
-
 delete = re.compile("(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
 replace_with_space = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
 
@@ -43,60 +31,113 @@ def preprocessing(reviews):
 
         return reviews  
 
-#lists positive words from Bing Liu's opinion lexicon
+#splits into train and validation sets
+reviews_train = reviews[0:2500];
+target_train = target[0:2500];
+reviews_valid = reviews[0:2500];
+target_valid = target[2500:3000];
+
+#reads positive words from Bing Liu's opinion lexicon
 text_file = open("positive-words.txt")
 pos_words = text_file.read().split()
-print(pos_words)
 text_file.close()
 
-import nltk.sentiment as nltk
-#ocument = 'a+ abound abounds abundance acclamation'.split()
-#nigrams = nltk.util.extract_unigram_feats(document, pos_words)
-#print(unigrams[1])
+#reads negative words
+text_file = open("negative-words.txt")
+neg_words = text_file.read().split()
+text_file.close()
 
-#Finds probability of y
+#Combines positive and negative words into one list
+lexicon = pos_words + neg_words
+
+#Splits training reviews into list of positive and negative reviews
 pos_reviews = []
 neg_reviews = []
 counter = 0
-for i in target:
-  if i:
+for i in range(len(target_train)):
+  if target_train[i]:
     counter = counter + 1
-    pos_reviews.append(reviews[i])
+    pos_reviews.append(reviews_train[i])
   else:
-    neg_reviews.append(reviews[i])
-Py = counter/len(target)
+    neg_reviews.append(reviews_train[i])
+#Finds probability review is positive
+Py1 = counter/float(len(target_train))
+Py0 = 1 - Py1
 
 #Finds P(xj = 1 | y =1) and P(xj = 0|y =1)
-p_11 = [0] * len(pos_words)
-p_01 = [0] * len(pos_words)
-reviews_test = []
-print(len(reviews))
-for i in range(25):
-  reviews_test.append(pos_reviews[i])
-for i in reviews_test:
-  unigrams = nltk.util.extract_unigram_feats(i,pos_words)
-  for j in range(len(pos_words)):
-    if (unigrams['contains('+pos_words[j]+')']):
-      p_11[j] = p_pos[j] + 1
+p_11 = [0] * len(lexicon)
+p_01 = [0] * len(lexicon)
+for i in pos_reviews:
+  unigrams = nltk.util.extract_unigram_feats(i.split(),lexicon) #returns dictionary {contains(word): true or false} for each review
+  for j in range(len(lexicon)):
+    if (unigrams['contains('+lexicon[j]+')']):		
+      p_11[j] = p_11[j] + 1
     else:
-      p_01[j] = p_pos[j] + 1
-      
-p_11 = [x / len(reviews_test) for x in p_11]
-p_01 = [x / len(reviews_test) for x in p_01]
+      p_01[j] = p_01[j] + 1
 
-#Finds P(xj = 1 | y =0 and P(xj = 0|y =0
-p_00 = [0] * len(pos_words)
-p_10 = [0] * len(pos_words)
-reviews_test = []
-for i in range(25):
-  reviews_test.append(neg_reviews[i])
-for i in reviews_test:
-  unigrams = nltk.util.extract_unigram_feats(i,pos_words)
-  for j in range(len(pos_words)):
-    if (unigrams['contains('+pos_words[j]+')']):
-      p_10[j] = p_pos[j] + 1
+#compute probabilities      
+p_11 = [(x + 1)/float((len(pos_reviews)+2)) for x in p_11]	#Laplace smoothed 
+p_01 = [x / float(len(pos_reviews)) for x in p_01]
+
+#Finds P(xj = 1 | y =0 and P(xj = 0|y =0)
+p_00 = [0] * len(lexicon)
+p_10 = [0] * len(lexicon)
+for i in neg_reviews:
+  unigrams = nltk.util.extract_unigram_feats(i.split(),lexicon)
+  for j in range(len(lexicon)):
+    if (unigrams['contains('+lexicon[j]+')']):
+      p_10[j] = p_10[j] + 1
     else:
-      p_00[j] = p_pos[j] + 1
+      p_00[j] = p_00[j] + 1
 
-p_10 = [x / len(reviews_test) for x in p_10]
-p_00= [x / len(reviews_test) for x in p_00]
+p_10 = [(x+1)/ float((len(neg_reviews)+2)) for x in p_10]	#compute probabilities
+p_00= [x / float(len(neg_reviews)) for x in p_00]
+
+
+#Makes decision boundary and classifies validation examples
+predict = [0] * len(reviews_valid)
+for i in range(len(reviews_valid)):
+  unigrams = nltk.util.extract_unigram_feats(reviews_valid[i].split(),lexicon)
+  boundary =  math.log10(Py1/Py0)
+  for j in range(len(lexicon)):
+    x_j = unigrams['contains('+lexicon[j]+')'];
+    w_j0 = math.log10(p_01[j]/p_00[j])
+    w_j1 = math.log10(p_11[j]/p_10[j])
+    boundary = boundary + w_j0 + (w_j1 - w_j0)*x_j
+  
+  if boundary > 0:
+    predict[i] = 1
+
+#Compares prediction with validation set
+TP = 0
+FP = 0
+TN = 0
+FN = 0
+for i in target_valid:
+  if i == 1:
+    if predict[i] == 1:
+      TP = TP + 1
+    else:
+      FN = FN + 1
+  else:
+    if predict[i] == 1:
+      FP = FP + 1
+    else:
+      TN = TN + 1
+print("True positives: ",TP)
+print("True negatives: ",TN)
+print("False positives: ",FP)
+print("False negatives: ",FN)
+
+Error_rate = (FP + FN)/float(len(reviews_valid))
+Accuracy = (TP + TN)/float(len(reviews_valid))
+Precision = TP/float((TP + FP))
+Recall = TP/float((TP + FN))
+Specificity = TN/float((FP + TN))
+
+print("Error Rate: ", Error_rate)
+print("Accuracy: ", Accuracy)
+print("Precision: ",Precision)
+print("Recall: ",Recall)
+print("Specificity: ",Specificity)
+
